8e977d779588e7b98ac6b57cd8d48e90a894d895
ttae0123 <xodnr1893@gmail.com>
2025-09-28 21:04:47 +0900

    Article dto 만들기(id는 추후를 위해 미리 추가)

--- src/main/java/com/example/mini_board/dto/ArticleForm.java
+++ src/main/java/com/example/mini_board/dto/ArticleForm.java
@@ -1,4 +1,12 @@
 package com.example.mini_board.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
 public class ArticleForm {
+    private Long id;
+    private String title;
+    private String content;
 }
