d1396d60d5a2683368b092edd518d140aa385161
ttae0123 <xodnr1893@gmail.com>
2025-09-28 20:42:22 +0900

    기본적인 헤더,푸터 생성 + 작동 테스트를 위한 컨트롤러코드 작성

--- src/main/java/com/example/mini_board/controller/ArticleController.java
+++ src/main/java/com/example/mini_board/controller/ArticleController.java
@@ -1,4 +1,20 @@
 package com.example.mini_board.controller;
 
+import com.example.mini_board.entity.Article;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@Slf4j
 public class ArticleController {
+
+    @GetMapping("/articles")
+    public String list() {
+        log.info("list");
+        return "articles/list";
+    }
 }
