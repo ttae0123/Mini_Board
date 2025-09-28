6f32019b29e2623ec4b28744a7c8c38b6fc20cbb
ttae0123 <xodnr1893@gmail.com>
2025-09-28 21:08:47 +0900

    레포지토리에 넣을 엔티티 폼 만들기 (id는 자동생성)

--- src/main/java/com/example/mini_board/entity/Article.java
+++ src/main/java/com/example/mini_board/entity/Article.java
@@ -1,4 +1,20 @@
 package com.example.mini_board.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@Entity
 public class Article {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String title;
+    @Column
+    private String content;
 }
