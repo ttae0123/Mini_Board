935022a7c967f8670c1b8ba698ba45e1f42baa0c
ttae0123 <xodnr1893@gmail.com>
2025-09-28 21:11:25 +0900

    레포지토리 인터페이스 선언

--- src/main/java/com/example/mini_board/repository/ArticleRepository.java
+++ src/main/java/com/example/mini_board/repository/ArticleRepository.java
@@ -1,4 +1,7 @@
 package com.example.mini_board.repository;
 
-public interface ArticleRepository {
+import com.example.mini_board.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleRepository extends JpaRepository<Article,Long> {
 }
