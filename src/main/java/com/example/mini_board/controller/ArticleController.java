a7c099394ae63a77aed2fef4ad175d849d1c687a
ttae0123 <xodnr1893@gmail.com>
2025-09-28 20:55:10 +0900

    컨트롤러 전체적인 틀 잡기

--- src/main/java/com/example/mini_board/controller/ArticleController.java
+++ src/main/java/com/example/mini_board/controller/ArticleController.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -12,9 +14,40 @@ import java.util.List;
 @Slf4j
 public class ArticleController {
 
-    @GetMapping("/articles")
-    public String list() {
+    @GetMapping("/articles") //글 목록
+    public String listArticles() {
         log.info("list");
         return "articles/list";
     }
+
+    @GetMapping("/articles/new") //작성 폼
+    public String newArticle(){
+        return "articles/new";
+    }
+
+    @PostMapping("/articles/new") //글 저장 후 목록 리다이렉트
+    public String createArticle(){
+        return "redirect:/articles/";
+    }
+
+    @GetMapping("articles/{id}") //글 상세 조회
+    public String detailArticle(@PathVariable String id){
+        return "articles/detail";
+    }
+
+    @GetMapping("articles/{id}/edit") //글 수정 폼
+    public String editArticle(@PathVariable String id){
+        return "articles/edit";
+    }
+
+    @PostMapping("articles/{id}/edit") //글 수정 후 상세 페이지 리다이렉트
+    public String updateArticle(@PathVariable String id){
+        return "redirect:/articles/";//+id
+    }
+
+    @PostMapping("articles/{id}/delete") //글 삭제 후 목록 리다이렉트
+    public String deleteArticle(@PathVariable String id){
+        return "redirect:/articles/";
+    }
+
 }
