4ebbcaa951e258955402df1b87219073d3ba6790
ttae0123 <xodnr1893@gmail.com>
2025-09-28 22:13:11 +0900

    수정 뷰 생성 + 컨트롤러 생성 + 그에 따른 수정

--- src/main/java/com/example/mini_board/controller/ArticleController.java
+++ src/main/java/com/example/mini_board/controller/ArticleController.java
@@ -22,7 +22,7 @@ public class ArticleController {
         this.articleRepository = articleRepository;
     }
 
-    @GetMapping("/articles") //글 목록
+    @GetMapping("/articles") //글 목록 O
     public String listArticles(Model model) {
         List<Article> articleEntityList = articleRepository.findAll();
         model.addAttribute("articleList", articleEntityList);
@@ -41,22 +41,32 @@ public class ArticleController {
         return "redirect:/articles";
     }
 
-    @GetMapping("articles/{id}") //글 상세 조회
+    @GetMapping("articles/{id}") //글 상세 조회 O
     public String detailArticle(@PathVariable Long id,Model model){
         Article articleEntity = articleRepository.findById(id).orElse(null);
         model.addAttribute("article", articleEntity);
-        log.info(articleEntity.toString());
         return "articles/detail";
     }
 
     @GetMapping("articles/{id}/edit") //글 수정 폼
-    public String editArticle(@PathVariable String id){
+    public String editArticle(@PathVariable Long id,Model model){
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", articleEntity);
+        log.info("수정요청");
         return "articles/edit";
     }
 
     @PostMapping("articles/{id}/edit") //글 수정 후 상세 페이지 리다이렉트
-    public String updateArticle(@PathVariable Long id){
-        return "redirect:/articles";//+id
+    public String updateArticle(@PathVariable Long id,ArticleForm form){
+        Article target = articleRepository.findById(id).orElse(null);
+        if (target != null) {
+            target.setTitle(form.getTitle());
+            target.setContent(form.getContent());
+            target.setAuthor(form.getAuthor());
+            articleRepository.save(target);
+        }
+        log.info(target.toString());
+        return "redirect:/articles/" + id;
     }
 
     @PostMapping("articles/{id}/delete") //글 삭제 후 목록 리다이렉트
--- src/main/java/com/example/mini_board/dto/ArticleForm.java
+++ src/main/java/com/example/mini_board/dto/ArticleForm.java
@@ -2,10 +2,16 @@ package com.example.mini_board.dto;
 
 import com.example.mini_board.entity.Article;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @ToString
+@Getter
+@Setter
 public class ArticleForm {
     private Long id;
     private String title;
